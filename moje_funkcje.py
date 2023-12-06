98dd05fed0bfc5c575e55ee32e5fb51b04a32b80
albim72 <marcin.albiniak72@gmail.com>
2023-12-06 11:35:16 +0100

    Update moje_funkcje.py

--- moje_funkcje.py
+++ moje_funkcje.py
@@ -1,4 +1,6 @@
 #przykład 1
+import math
+
 n=100
 
 def hx(k):
@@ -12,3 +14,13 @@ def policz(a,b,c,y):
 print(policz(56,3,7,88))
 print(policz(5.07,13,4.5,-0.88))
 print(n)
+
+#przykład 2
+
+def wx(a,d,b=1,c=8):
+    return math.sqrt(a*b*c)+d
+
+print(wx(5,7,9,4))
+print(wx(5,7,4))
+print(wx(5,5))
+print(wx(3,7,c=51))
