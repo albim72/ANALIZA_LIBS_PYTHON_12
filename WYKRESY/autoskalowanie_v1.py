b97a639401bea05035b086c216db35ab12032981
albim72 <marcin.albiniak72@gmail.com>
2023-12-08 09:28:33 +0100

    Add files via upload

---
+++ WYKRESY/autoskalowanie_v1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# -- Sheet --
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+x=np.linspace(-2*np.pi,2*np.pi,100)
+y = np.sinc(x)
+
+fig,ax = plt.subplots(ncols=2,figsize=(24,16))
+ax[0].plot(x,y)
+ax[0].set_title('pojedyncza krzywa')
+ax[1].plot(x,y)
+ax[1].plot(x*2.0,y)
+ax[1].set_title('Dwie krzywe')
+plt.show()
+
